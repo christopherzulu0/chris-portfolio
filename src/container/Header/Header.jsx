f986409611a610362ddb7f42c48f5d1cae31d85c
christopherzulu0 <christopherzulu04@gmail.com>
2022-09-19 04:51:07 +0200

    Data Add

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -34,6 +34,7 @@ const Header = () => (
 
         <div className="tag-cmp app__flex">
           <p className="p-text">Web Developer</p>
+          <p className="p-text">Web Designer</p>
           <p className="p-text">Freelancer</p>
         </div>
       </div>
