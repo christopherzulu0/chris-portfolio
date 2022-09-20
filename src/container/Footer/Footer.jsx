81f829af4d8a38c3faf6bf61e8d4e0786b9c8c46
christopherzulu0 <christopherzulu04@gmail.com>
2022-09-20 15:51:18 +0200

    contact form data muted for the inputs

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,55 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import { images } from '../../constants';
 import { AppWrap, MotionWrap } from '../../wrapper';
-import { client } from '../../client';
+// import { client } from '../../client';
 import './Footer.scss';
 
-const Footer = () => {
-  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
-  const [isFormSubmitted, setIsFormSubmitted] = useState(false);
-  const [loading, setLoading] = useState(false);
+const Footer = () => (
+  <>
+    <h2 className="head-text">Take a coffee & chat with me</h2>
 
-  const { username, email, message } = formData;
-
-  const handleChangeInput = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = () => {
-    setLoading(true);
-
-    const contact = {
-      _type: 'contact',
-      name: formData.username,
-      email: formData.email,
-      message: formData.message,
-    };
-
-    client.create(contact)
-      .then(() => {
-        setLoading(false);
-        setIsFormSubmitted(true);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  return (
-    <>
-      <h2 className="head-text">Take a coffee & chat with me</h2>
-
-      <div className="app__footer-cards">
-        <div className="app__footer-card ">
-          <img src={images.email} alt="email" />
-          <a href="mailto:christopherzulu04@gmail.com" className="p-text">christopherzulu04@gmail.com</a>
-        </div>
-        <div className="app__footer-card">
-          <img src={images.mobile} alt="phone" />
-          <a href="tel:+260971488377" className="p-text">+260971488377</a>
-        </div>
+    <div className="app__footer-cards">
+      <div className="app__footer-card ">
+        <img src={images.email} alt="email" />
+        <a href="mailto:christopherzulu04@gmail.com" className="p-text">christopherzulu04@gmail.com</a>
       </div>
-      {!isFormSubmitted ? (
+      <div className="app__footer-card">
+        <img src={images.mobile} alt="phone" />
+        <a href="tel:+260971488377" className="p-text">+260971488377</a>
+      </div>
+    </div>
+    {/* {!isFormSubmitted ? (
         <div className="app__footer-form app__flex">
           <div className="app__flex">
             <input className="p-text" type="text" placeholder="Your Name" name="username" value={username} onChange={handleChangeInput} />
@@ -74,11 +44,9 @@ const Footer = () => {
             Thank you for getting in touch!
           </h3>
         </div>
-      )}
-    </>
-  );
-};
-
+      )} */}
+  </>
+);
 export default AppWrap(
   MotionWrap(Footer, 'app__footer'),
   'contact',
